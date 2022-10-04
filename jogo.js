75ac0016f2c5e75444735331a4ac8a084017856f
guilhermewt <welliston10@gmail.com>
2022-10-03 22:46:01 -0300

    adicionado baloes com o for e colando na tela

--- jogo.js
+++ jogo.js
@@ -17,6 +17,24 @@ function iniciarJogo(){
 	if(nivel_jogo == 3){ // 3 dificil -> 30segundos
 		tempo_segundos = 30;
 	}
-
+	//inserindo segundos no span
 	document.getElementById('cronometro').innerHTML = tempo_segundos;
+
+	//quantidade de baloes
+	var qtde_baloes = 20;
+	cria_baloes(qtde_baloes);
+
+	//imprimir qtde baloes inteiros
+	document.getElementById('baloes_inteiros').innerHTML = qtde_baloes;
+	document.getElementById('baloes_estourados').innerHTML = 0;
+}
+
+function cria_baloes(qtde_baloes){
+	for(var i = 1; i <= qtde_baloes; i++){
+		var balao = document.createElement("img");
+		balao.src = 'imagens/balao_azul_pequeno.png';
+		balao.style.margin = '10px';
+
+		document.getElementById('cenario').appendChild(balao);
+	}
 }
